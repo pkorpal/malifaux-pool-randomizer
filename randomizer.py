74020950579e6febb4383ad97f10b6a2f49e7a95
Pawel Korpal <pawelkorpal@macbook-air-pawel.home>
2024-08-21 23:40:28 +0200

    more optimization

--- randomizer.py
+++ randomizer.py
@@ -2,35 +2,29 @@ import json
 import random
 import qrcode
 from PIL import Image, ImageDraw, ImageFont
-
+from datetime import datetime
 
 def read_params(filename='params.json'):
     with open(filename, 'r') as file:
-        data = json.load(file)
-    return data
+        return json.load(file)
 
 def read_gg(gg_num):
     with open(f'gg{gg_num}.json', 'r') as file:
-        data = json.load(file)
-    return data
+        return json.load(file)
 
 def randomize_round(gg, round_num):
-    selected_strategy = gg['strategies'].pop()
-    selected_deployment = gg['deployments'].pop()
+    selected_strategy = random.choice(gg['strategies'])
+    selected_deployment = random.choice(gg['deployments'])
     selected_schemes = random.sample(gg['schemes'], 5)
 
-    pool = {
-        "Name": "Round " + str((round_num+1)),
+    return {
+        "Name": f"Round {round_num + 1}",
         "Strategy": selected_strategy,
         "Deployment": selected_deployment,
         "Schemes": selected_schemes
     }
-    
-    return pool
 
-def generate_qr(round, app_ver, max_crew_size, index):
-    font = ImageFont.truetype("/System/Library/Fonts/Helvetica.ttc", 40)
-
+def generate_qr(round, app_ver, max_crew_size, index, font):
     data_input = {
         "specialRules": {"Singles": {"name": "Singles", "value": None}},
         "name": round["Name"],
@@ -39,13 +33,12 @@ def generate_qr(round, app_ver, max_crew_size, index):
         "deployment": round["Deployment"],
         "maxCrewSize": max_crew_size,
         "createdIn": app_ver,
-        "created": "2024-08-21T21:37:00.000",
+        "created": datetime.now().isoformat(),  # Use current date and time
         "schemePool": round["Schemes"]
     }
-    qr_img = qrcode.make(json.dumps(data_input))
-
-    img0 = Image.new('RGB', (795, 1520), color='white')
     
+    qr_img = qrcode.make(json.dumps(data_input))
+    img0 = Image.new('RGB', (795, 1520), color='white')
     img0.paste(qr_img, (0, 730))
 
     draw = ImageDraw.Draw(img0)
@@ -57,16 +50,19 @@ def generate_qr(round, app_ver, max_crew_size, index):
         y = 300 + i * 60
         draw.text((50, y), scheme, fill='black', font=font)
 
-    img_name = f"img{1 + index}.png"
+    img_name = f"img{index + 1}.png"
     img0.save(img_name)
 
 def main():
     params = read_params()
     gg = read_gg(params['gg'])
+    
+    # Pre-load the font once
+    font = ImageFont.truetype("/System/Library/Fonts/Helvetica.ttc", 40)
+    
     rounds = [randomize_round(gg, n) for n in range(params['rounds'])]
-    for index,round in enumerate(rounds):
-        generate_qr(round, params['app_ver'], params['max_crew_size'], index)
+    for index, round in enumerate(rounds):
+        generate_qr(round, params['app_ver'], params['max_crew_size'], index, font)
 
 if __name__ == '__main__':
     main()
-    
