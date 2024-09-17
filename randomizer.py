51f23052baa8555a8d9e81ae41f10c825cfa9323
Pawel Korpal <pawelkorpal@MacBook-Pro.local>
2024-09-17 10:44:46 +0200

    fixed qr code issue

--- randomizer.py
+++ randomizer.py
@@ -4,79 +4,74 @@ import qrcode
 from PIL import Image, ImageDraw, ImageFont
 from datetime import datetime
 
-def read_params(filename='params.json'):
-    with open(filename, 'r') as file:
+
+def read_params(filename="params.json"):
+    with open(filename, "r") as file:
         return json.load(file)
+
 
 def read_gg(gg_num):
-    with open(f'gg{gg_num}.json', 'r') as file:
+    with open(f"gg{gg_num}.json", "r") as file:
         return json.load(file)
 
-def randomize_round_no_duplicates(gg, round_num, previous_deployments):
-    selected_strategy = random.choice(gg['strategies'])
-    
-    # Ensure no duplicate deployments if rounds are fewer than 4
-    if round_num < 3:
-        available_deployments = [dep for dep in gg['deployments'] if dep not in previous_deployments]
-    else:
-        available_deployments = gg['deployments']
-    
-    selected_deployment = random.choice(available_deployments)
-    selected_schemes = random.sample(gg['schemes'], 5)
 
+def randomize_round(gg, round_num, event_name):
+    selected_strategy = random.choice(gg["strategies"])
+    selected_deployment = random.choice(gg["deployments"])
+    selected_schemes_ids = random.sample(range(0, 13), 5)
+    print(selected_schemes_ids)
+    # selected_schemes_names = random.sample(gg["schemes"], 5)
+    selected_schemes_names = [gg["schemes"][i] for i in selected_schemes_ids]
     return {
-        "Name": f"Round {round_num + 1}",
+        "Name": f"{event_name} #{round_num + 1}",
         "Strategy": selected_strategy,
         "Deployment": selected_deployment,
-        "Schemes": selected_schemes
-    }, selected_deployment
+        "Schemes": selected_schemes_names,
+        "Schemes_ids": selected_schemes_ids,
+    }
 
-def generate_qr(round, app_ver, max_crew_size, index, font, event_name):
+
+def generate_qr(round, app_ver, max_crew_size, index, font):
     data_input = {
         "specialRules": {"Singles": {"name": "Singles", "value": None}},
         "name": round["Name"],
-        "ruleset": f"{event_name} #{index+1}",
+        "ruleset": "GG Season 4",
         "strat": round["Strategy"],
         "deployment": round["Deployment"],
         "maxCrewSize": max_crew_size,
         "createdIn": app_ver,
         "created": datetime.now().isoformat(),  # Use current date and time
-        "schemePool": round["Schemes"]
+        "schemePool": round["Schemes_ids"],
     }
-    
+
     qr_img = qrcode.make(json.dumps(data_input))
-    img0 = Image.new('RGB', (795, 1520), color='white')
+    img0 = Image.new("RGB", (795, 1520), color="white")
     img0.paste(qr_img, (0, 730))
 
     draw = ImageDraw.Draw(img0)
-    draw.text((40, 40), f"{event_name} #{index+1}", fill='black', font=font)
-    draw.text((50, 120), round["Strategy"], fill='black', font=font)
-    draw.text((50, 180), round["Deployment"], fill='black', font=font)
+    draw.text((40, 40), round["Name"], fill="black", font=font)
+    draw.text((50, 120), round["Strategy"], fill="black", font=font)
+    draw.text((50, 180), round["Deployment"], fill="black", font=font)
 
     for i, scheme in enumerate(round["Schemes"]):
         y = 300 + i * 60
-        draw.text((50, y), scheme, fill='black', font=font)
+        draw.text((50, y), scheme, fill="black", font=font)
 
     img_name = f"img{index + 1}.png"
     img0.save(img_name)
 
+
 def main():
     params = read_params()
-    gg = read_gg(params['gg'])
-    
+    gg = read_gg(params["gg"])
+
     # Pre-load the font once
     font = ImageFont.truetype("/System/Library/Fonts/Helvetica.ttc", 40)
-    
-    previous_deployments = []
-    rounds = []
-    
-    for n in range(params['rounds']):
-        round_data, selected_deployment = randomize_round_no_duplicates(gg, n, previous_deployments)
-        previous_deployments.append(selected_deployment)  # Track used deployments to avoid duplicates
-        rounds.append(round_data)
-        
-        # Generate QR and image for each round
-        generate_qr(round_data, params['app_ver'], params['max_crew_size'], n, font, params['event_name'])
 
-if __name__ == '__main__':
+    rounds = [randomize_round(gg, n, params["event_name"]) for n in range(params["rounds"])]
+    for index, round in enumerate(rounds):
+        generate_qr(round, params["app_ver"], params["max_crew_size"], index, font)
+
+
+if __name__ == "__main__":
     main()
